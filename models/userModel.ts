798ba89c4f3d88cee1fc709e4c54351f2796327e
musaa-muhsen <musaamuhsen@gmail.com>
2023-06-30 14:00:17 +0100

    update

--- models/userModel.ts
+++ models/userModel.ts
@@ -12,6 +12,7 @@ const findAll = (): Promise<User[]> => {
 const create = (user: User): Promise<string> => {
     return new Promise((resolve, reject) => {
         users.push(user);
+        console.log('user id',user.id)
         resolve(`User with the id of ${user.id} has been added to the database 🤖.`)
     })
 }
@@ -23,9 +24,39 @@ const findById = (id: string): Promise<User | undefined> => {
     })
 }
 
+const removeById = (id: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        users = users.filter(user => user.id !== id);
+        resolve(`User: ${id} is deleted from database.`)
+    })
+}
+
+const updateById = (
+      id: string, 
+      updatedUser: Partial<User>
+    ): Promise<string> => {
+        return new Promise((resolve,reject) => {
+            const userIndex = users.findIndex(user => user.id === id);
+            if (userIndex === -1){
+                reject('User not found');
+                return;
+            }
+
+            console.log('users[userIndex]', users[userIndex])
+            console.log('updatedUser', updatedUser)
+
+            users[userIndex] = {...users[userIndex], ...updatedUser}
+
+            resolve(`User with id ${id} has been updated.`)
+
+        })
+}
+
 
 export default {
     findAll,
     create,
-    findById
+    findById,
+    removeById,
+    updateById
 }
--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,13 @@ const PORT: number = 5007;
 // initilize body parser middleware 
 // this is going to tell the script that we're going to use json data in our whole application
 // JSON = javascript object notation - each key/value needs to be wrapped in quatation marks
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // you don't have to call next() here bc 
 
+app.use(function (req, res, next) {
+  // console.log(typeof req)
+    console.log('Time:', Date.now());
+    next();
+  });
 // we set all the starting paths for all the routes in the users file
 // all paths are starting with /users
 app.use('/users', usersRoutes)
@@ -20,6 +25,8 @@ app.use('/users', usersRoutes)
 //     res.send('Hello from homepage')
 // })
 // 
+
+
 app.listen(PORT, () =>
 console.log(`Server running on port: http://localhost:${PORT} `) )
 
